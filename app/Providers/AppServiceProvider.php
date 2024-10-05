65666a676382a4c1a017780e18d2bcd83ee329e4
PopovicJovan <jovanp.school06@gmail.com>
2024-10-05 08:34:55 +0200

    nowrap method added in resources

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Resources\Car\CarResource;
+use App\Http\Resources\RateResource;
+use App\Http\Resources\ReservationResource;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        CarResource::withoutWrapping();
+        RateResource::withoutWrapping();
+        ReservationResource::withoutWrapping();
+        UserResource::withoutWrapping();
     }
 }
